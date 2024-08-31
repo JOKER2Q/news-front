7c05db0c57fcf4dd8a08f591d724f176fa3c04ee
JOKER2Q <rinas1122k@gmail.com>
2024-08-31 01:40:36 -0700

    HI DIYAR

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <>
       <h1>HELLO WORLD!</h1>
       <About />
-
+      <Contact />
       <Footer />
     </>
   );
